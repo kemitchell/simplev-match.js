5b073fc16a0cdea133efa1e69cb18ef33f510a23
Kyle Mitchell <kyle@kemitchell.com>
2016-01-20 15:18:59 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+```javascript
+var match = require('simplev-match')
+var assert = require('assert')
+
+assert( match('1.1.1',   '1.1.1'))
+assert( match('1.1.0',   '1.1.1'))
+assert( match('1.0.0',   '1.1.1'))
+assert( match('1.0.0-1', '1.0.0'))
+
+assert(!match('2.0.0',   '1.1.1'))
+assert(!match('2.0.0',   '1.1.1'))
+assert(!match('1.1.2',   '1.1.1'))
+assert(!match('1.0.0-2', '1.0.0-1'))
+
+assert.throws(
+  function() {
+    match('not a simplev', '1.0.0') },
+  /Invalid requirement string "not a simplev"$/)
+
+assert.throws(
+  function() {
+    match('1.0.0', 'not a simplev') },
+  /Invalid simplev string "not a simplev"$/)
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = simplevMatch
+
+var parse = require('simplev-parse')
+
+// Components of a simplev, in descending order of precedence.
+var components = [ 'edition', 'revision', 'correction', 'draft' ]
+
+function simplevMatch(requirementString, simplevString) {
+  // First argument must be a valid simplev.
+  var requirement = parse(requirementString)
+  if (!requirement) {
+    throw new Error(
+      'Invalid requirement string' +
+      ' "' + requirementString + '"') }
+  // Second argument must be a valid simplev.
+  var simplev = parse(simplevString)
+  if (!simplev) {
+    throw new Error(
+      'Invalid simplev string' +
+      ' "' + simplevString + '"') }
+  // Does the simplev match the requirement?
+  return components.every(function(c) {
+    return (
+      !simplev.hasOwnProperty(c) ||
+      parseInt(requirement[c]) <= parseInt(simplev[c]) ) }) }
